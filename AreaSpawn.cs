ddd67831d83f976d91ebb14b3a0e301f7bd1f43c
BrightDanny <66916960+BrightDanny@users.noreply.github.com>
2020-06-14 19:09:49 +0200

    Add files via upload

---
+++ AreaSpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaSpawn : MonoBehaviour
+{
+    public Vector3 curPos;
+
+    public int areaAmount;
+    public int rows;
+
+    public int totalArea;
+
+    //spriterenderer layer order
+    public int layer;
+
+    //offsets
+    public float xOffset;
+    public float yOffset;
+
+    //set the delay between spawns
+    public float spawnSpeed;
+
+
+
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+
+    IEnumerator Spawn()
+    {
+
+        curPos = transform.position;
+        totalArea = rows * areaAmount;
+
+        layer++;
+        //Vertical
+        for (int row = 0; row < rows; row++)
+        {
+            for (int i = 0; i < areaAmount; i++)
+            {
+                yield return new WaitForSeconds(spawnSpeed);
+                GameObject _area = Instantiate(Resources.Load("BuildArea") as GameObject);
+                _area.transform.position = curPos;
+                _area.transform.parent = gameObject.transform;
+                curPos = new Vector3(curPos.x + xOffset, curPos.y + yOffset, 0);
+                _area.GetComponentInChildren<SpriteRenderer>().sortingOrder = layer;
+                layer++;
+            }
+            curPos = new Vector3(curPos.x - xOffset * areaAmount, curPos.y - yOffset * areaAmount, 0);
+
+            curPos = new Vector3(curPos.x + xOffset, curPos.y - yOffset, 0);
+        }
+    }
+}
